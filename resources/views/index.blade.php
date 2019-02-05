6135caa4b0988eaaa4501710fc95c7d0164304ed
unknown <boume.taki@gmail.com>
2019-02-05 23:21:59 +0100

    initial 2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.base')
+
+@section('title', 'Page Title')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-between mb-2">
+            <div class="col">
+                <h4>Catégorie</h4>
+            </div>
+            <div class="col text-right">
+                <a class="btn btn-primary" href="{{ route('index') }}">Ajouter</a>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Libelle</th>
+                <th scope="col">Type du mouvement</th>
+                <th scope="col">Modifier</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Yep</td>
+                    <td>Yo !</td>
+
+                    <td><a href="{{ route('index') }}" class="btn btn-primary">Modifier</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mouvements - @yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,600" rel="stylesheet" type="text/css">
+    <style>
+        h4 {
+            font-family: 'Raleway', sans-serif;
+            font-weight: 600;
+            color: orangered;
+        }
+
+        table {
+            font-size: 1em;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 300;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('index') }}">Budget</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mouvements</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Categorie</a>
+                </li>
+            </ul>
+            {{--<form class="form-inline my-2 my-lg-0">--}}
+            {{--<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">--}}
+            {{--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>--}}
+            {{--</form>--}}
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="mt-4">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @yield('content')
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</body>
+</html>
